585c2bd08af133386c6aa1ead5042244272787a6
Yuriy Tymchyna <ytymchyna@gmail.com>
2020-05-18 10:28:36 +0300

    add scripts in home work moudule-01

---
+++ js/task-05.js
@@ -0,0 +1,61 @@
+/*
+	Пользователь может оформить доставку товара к себе в страну, указав ее при посещении страницы в prompt. Учти, пользователь может ввести имя страны не только буквами нижнего регистра, а к примеру 'кИтАЙ'.
+
+	Напиши скрипт который выводит сообщение о стоимости доставки в указанную страну. Обязательно используй switch. Формат сообщения: 'Доставка в [страна] будет стоить [цена] кредитов'.
+
+	Но доставка есть не везде, если указанной страны нет в списке, то выводи в alert сообщение 'В вашей стране доставка не доступна'.
+
+	Ниже приведен список стран и стоимость доставки.
+
+		* Китай - 100 кредитов
+		* Чили - 250 кредитов
+		* Австралия - 170 кредитов
+		* Индия - 80 кредитов
+		* Ямайка - 120 кредитов
+*/
+
+const chinaDeliveryPrice = 100;
+const chiliDeliveryPrice = 250;
+const australiaDeliveryPrice = 170;
+const indiaDeliveryPrice = 80;
+const jamaicaDeliveryPrice = 120;
+
+const userCountryChoice = prompt(
+  'Пожалуйста, укажите страну куда необходимо выполнить доставку',
+);
+
+if (!userCountryChoice) {
+  alert('Отменено пользователем!');
+}
+
+const countryInLowerCase = userCountryChoice.toLocaleLowerCase();
+
+switch (countryInLowerCase) {
+  case 'китай':
+    alert(
+      `Доставка в ${countryInLowerCase()} будет стоить ${chinaDeliveryPrice} кредитов'`,
+    );
+    break;
+  case 'чили':
+    alert(
+      `Доставка в ${countryInLowerCase} будет стоить ${chiliDeliveryPrice} кредитов'`,
+    );
+    break;
+  case 'австралия':
+    alert(
+      `Доставка в ${countryInLowerCase} будет стоить ${australiaDeliveryPrice} кредитов'`,
+    );
+    break;
+  case 'индия':
+    alert(
+      `Доставка в ${countryInLowerCase} будет стоить ${indiaDeliveryPrise} кредитов'`,
+    );
+    break;
+  case 'ямайка':
+    alert(
+      `Доставка в ${countryInLowerCase} будет стоить ${jamaicaDeliveryPrice} кредитов'`,
+    );
+    break;
+  default:
+    alert(`В вашей стране доставка не доступна`);
+}
